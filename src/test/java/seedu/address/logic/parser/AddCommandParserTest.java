ac1c74a50fb6cee891165ed0536bcc2316ddbc93
tiongjjyi <tiongjjyi@gmail.com>
2023-10-16 14:20:30 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -37,7 +37,6 @@ import seedu.address.model.person.Course;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Student;
-import seedu.address.model.tag.Tag;
 import seedu.address.testutil.StudentBuilder;
 
 public class AddCommandParserTest {
