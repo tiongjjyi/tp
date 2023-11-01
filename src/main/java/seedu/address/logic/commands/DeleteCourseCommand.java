0eb5d0ccc07a11a71757580ca8454f7dc6cd170b
sunzihan23 <e0970553@u.nus.edu>
2023-11-01 21:50:54 +0800

    Edit delete message for course

--- src/main/java/seedu/address/logic/commands/DeleteCourseCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCourseCommand.java
@@ -24,7 +24,7 @@ public class DeleteCourseCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_COURSE_SUCCESS = "Deleted Course: %1$s";
+    public static final String MESSAGE_DELETE_COURSE_SUCCESS = "Deleted course: %1$s";
 
     private final Index targetIndex;
 
@@ -43,7 +43,7 @@ public class DeleteCourseCommand extends Command {
 
         Course courseToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deleteCourse(courseToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_STUDENT_SUCCESS, Messages.format(courseToDelete)));
+        return new CommandResult(String.format(MESSAGE_DELETE_COURSE_SUCCESS, Messages.format(courseToDelete)));
     }
 
     @Override
