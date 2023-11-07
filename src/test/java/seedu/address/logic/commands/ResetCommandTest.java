6b6b3857c27006832aab852c6281a5cbdb36e2ca
sunzihan23 <e0970553@u.nus.edu>
2023-11-07 19:17:18 +0800

    Add tests for ResetCommandTest

--- src/test/java/seedu/address/logic/commands/ResetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ResetCommandTest.java
@@ -1,23 +1,36 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showStudentAtIndex;
-import static seedu.address.testutil.TypicalStudents.getTypicalStudentList;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_COURSE;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
+import seedu.address.logic.parser.StageManager;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.course.Course;
-import seedu.address.testutil.CourseBuilder;
-import seedu.address.testutil.TypicalCourses;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.TypicalCourses.getTypicalCourseList;
+import static seedu.address.testutil.TypicalCourses.getTypicalCourses;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ResetCommand.
  */
 public class ResetCommandTest {
+    private Model model = new ModelManager(getTypicalCourseList(), new UserPrefs());
 
+    @Test
+    void execute_resetCommand_success() {
+        Course validCourse = getTypicalCourses().get(1);
+
+        StageManager stageManager = StageManager.getInstance();
+        stageManager.setCourseStage(validCourse);
+
+        CommandResult commandResult = new ResetCommand().execute(model);
+        String expectedMessage = ResetCommand.MESSAGE_SUCCESS;
+        CommandResult expectedResult = new CommandResult(expectedMessage);
+
+        assertEquals(commandResult, expectedResult);
+    }
 }
